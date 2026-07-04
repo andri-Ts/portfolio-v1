911fd3bc2d9460f376b905311cd1796649ce41e2
Po Bidau <pobidau@protonmail.com>
2026-07-05 00:23:44 +0200

    feat: creation contenu hero

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -5,7 +5,32 @@ import Section from '../../components/ui/section/Section';
 function Hero() {
   return (
     <Section id="hero" className={styles.hero}>
-      <h1>Hero</h1>
+      <div className={styles.container}>
+        <p className={styles.greeting}>Bonjour 👋</p>
+
+        <h1 className={styles.title}>Je suis Ticyano</h1>
+
+        <h2 className={styles.subtitle}>
+          <span className={styles.highlight}>[Développeur Web]</span> &
+          Ingénieur en Systèmes Embarqués
+        </h2>
+
+        <p className={styles.description}>
+          Je crée des solutions innovantes avec l’IA, les technologies web et
+          l’IoT. Passionné par la création d’applications utiles qui résolvent
+          des problèmes réels.
+        </p>
+
+        <div className={styles.actions}>
+          <a href="#projects" className={styles.primaryBtn}>
+            Voir les projets →
+          </a>
+
+          <a href="#contact" className={styles.secondaryBtn}>
+            Me contacter
+          </a>
+        </div>
+      </div>
     </Section>
   );
 }
