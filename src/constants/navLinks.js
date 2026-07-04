56be2713f001c364c4b09c6f5d435adc2795a792
Po Bidau <pobidau@protonmail.com>
2026-07-04 15:22:15 +0200

    feat: creation des constants pour navbar

---
+++ src/constants/navLinks.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    id: 'hero',
+    label: 'Accueil',
+  },
+  {
+    id: 'about',
+    label: 'À propos',
+  },
+  {
+    id: 'skills',
+    label: 'Compétences',
+  },
+  {
+    id: 'experience',
+    label: 'Expérience',
+  },
+  {
+    id: 'projects',
+    label: 'Projets',
+  },
+  {
+    id: 'contact',
+    label: 'Contact',
+  },
+];
---
+++ src/constants/socialLinks.js
@@ -0,0 +1,14 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export const socialLinks = [
+  {
+    icon: FaGithub,
+    url: 'https://github.com/',
+    label: 'Github',
+  },
+  {
+    icon: FaLinkedin,
+    url: 'https://linkedin.com/',
+    label: 'Linkedin',
+  },
+];
