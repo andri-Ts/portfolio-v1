70641dcbed2b3ebf70262cb7bbad994ea9881a2b
Po Bidau <pobidau@protonmail.com>
2026-07-05 21:27:49 +0200

    fix: maj de heroSection

--- src/sections/heroSection/HeroSection.jsx
+++ src/sections/heroSection/HeroSection.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './heroSection.module.css';
 import Section from '../../components/ui/section/Section';
 
+const roles = [
+  'Développeur Web',
+  'Ingénieur Logiciel',
+  'Ingénieur Système Embarqué',
+];
+
 function HeroSection() {
+  const [activeRole, setActiveRole] = useState(0);
+
+  //----------------------------
+  // Rotation automatique des mots
+  //---------------------------
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveRole((prevValue) => (prevValue + 1) % roles.length);
+    }, 2000); // change tous les 2s
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Section className={styles.heroSection}>
       <div className={styles.container}>
@@ -10,10 +29,23 @@ function HeroSection() {
 
         <h1 className={styles.title}>Je suis Ticyano</h1>
 
+        <div className={styles.roles}>
+          {roles.map((role, index) => (
+            <span
+              key={role}
+              className={`${styles.role} ${
+                activeRole === index ? styles.active : styles.inactive
+              }`}
+            >
+              {activeRole === index ? `[${role}]` : role}
+            </span>
+          ))}
+        </div>
+
         <p className={styles.description}>
           Je crée des solutions innovantes avec l’IA, les technologies web et
           l’IoT. Passionné par la création d’applications utiles qui résolvent
-          des problèmes réels.
+          des problèmes réels. Test
         </p>
 
         <div className={styles.actions}>
