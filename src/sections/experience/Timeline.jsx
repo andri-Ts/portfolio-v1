00a1f17a20fe2fee46ab62fef90b939d8fc4b34e
Po Bidau <pobidau@protonmail.com>
2026-07-13 20:32:31 +0200

    feat: ajout des css

--- src/sections/experience/Timeline.jsx
+++ src/sections/experience/Timeline.jsx
@@ -1,13 +1,14 @@
 import { experiences } from '../../constants/experiences';
 import TimelineItem from './TimelineItem';
+import styles from './experience.module.css';
 
 function Timeline() {
   return (
-    <>
+    <div className={styles.timeline}>
       {experiences.map((experience) => (
         <TimelineItem key={experience.id} experience={experience} />
       ))}
-    </>
+    </div>
   );
 }
 
--- src/sections/experience/TimelineItem.jsx
+++ src/sections/experience/TimelineItem.jsx
@@ -9,11 +9,11 @@ function TimelineItem({ experience }) {
       <div className={styles.content}>
         <h3 className={styles.jobTitle}>{experience.title}</h3>
 
-        <p className={styles.company}>{experience.company}</p>
+        <p className={styles.company}>
+          {experience.company} • {experience.location}
+        </p>
 
-        <p className={styles.location}>{experience.location}</p>
-
-        <p className={styles.description}>{experience.description}</p>
+        <p className={styles.descriptionText}>{experience.description}</p>
 
         <div className={styles.tags}>
           {experience.technologies.map((technology) => (
