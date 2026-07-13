b91fd53d8bdc53f37caab706685c121fbc4351ec
Po Bidau <pobidau@protonmail.com>
2026-07-13 21:01:30 +0200

    feat: maj 2 de seciton experience

--- src/sections/experience/Timeline.jsx
+++ src/sections/experience/Timeline.jsx
@@ -1,10 +1,35 @@
+import { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'motion/react';
+
 import { experiences } from '../../constants/experiences';
 import TimelineItem from './TimelineItem';
+
 import styles from './experience.module.css';
 
 function Timeline() {
+  const timelineRef = useRef(null);
+
+  // Progression du scroll sur toute la timeline
+  const { scrollYProgress } = useScroll({
+    target: timelineRef,
+    offset: ['start 20%', 'end 80%'],
+  });
+
+  // La ligne grandira de 0% à 100%
+  const lineHeight = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+
   return (
-    <div className={styles.timeline}>
+    <div ref={timelineRef} className={styles.timeline}>
+      {/* Ligne de fond */}
+      <div className={styles.lineBackground}>
+        {/* Ligne animée */}
+        <motion.div
+          className={styles.lineProgress}
+          style={{ height: lineHeight }}
+        />
+      </div>
+
+      {/* Les expériences */}
       {experiences.map((experience) => (
         <TimelineItem key={experience.id} experience={experience} />
       ))}
--- src/sections/experience/TimelineItem.jsx
+++ src/sections/experience/TimelineItem.jsx
@@ -1,12 +1,36 @@
 import styles from './experience.module.css';
 import Tag from '../../components/ui/tag/Tag';
+import { motion } from 'motion/react';
 
 function TimelineItem({ experience }) {
   return (
-    <article className={styles.timelineItem}>
-      <div className={styles.period}>{experience.period}</div>
+    <motion.article
+      className={styles.timelineItem}
+      initial={{
+        opacity: 0,
+        y: 40,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      viewport={{
+        once: true,
+        amount: 0.3,
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      {/* Point sur la timeline */}
+      <div className={styles.marker}>
+        <div className={styles.dot}></div>
+      </div>
 
+      {/* Contenu */}
       <div className={styles.content}>
+        <p className={styles.period}>{experience.period}</p>
+
         <h3 className={styles.jobTitle}>{experience.title}</h3>
 
         <p className={styles.company}>
@@ -21,7 +45,7 @@ function TimelineItem({ experience }) {
           ))}
         </div>
       </div>
-    </article>
+    </motion.article>
   );
 }
 
